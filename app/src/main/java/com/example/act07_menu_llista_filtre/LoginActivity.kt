f3922ed7594d60077ef9c87f45ac39e3f59215af
Estefania <rubianoestefania15@gmail.com>
2026-01-17 02:12:51 +0100

    si

--- app/src/main/java/com/example/act07_menu_llista_filtre/LoginActivity.kt
+++ app/src/main/java/com/example/act07_menu_llista_filtre/LoginActivity.kt
@@ -3,11 +3,8 @@ package com.example.act07_menu_llista_filtre
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var btnTestNav: Button
