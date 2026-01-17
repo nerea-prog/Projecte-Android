ca9c8cdf8ec4609f8ed902164dee6ef5b1fe8564
nerea-prog <narvaeznerea18@gmail.com>
2026-01-17 01:56:03 +0100

    Menus en procés

--- app/src/main/java/com/example/act07_menu_llista_filtre/RegistroCorrectoActivity.kt
+++ app/src/main/java/com/example/act07_menu_llista_filtre/RegistroCorrectoActivity.kt
@@ -2,21 +2,68 @@ package com.example.act07_menu_llista_filtre
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class RegistroCorrectoActivity : AppCompatActivity() {
     private lateinit var btnTestNav: Button
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_correcto)
-
+        setupToolbar()
         initComponents()
         initListeners()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Les Meves Tasques"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_inici -> {
+                Toast.makeText(this, "Anant al Inici", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainUserActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_configuracio -> {
+                Toast.makeText(this, "Anant a configuració", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ConfigurationActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_sobre -> {
+                mostrarDialogSobre()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun mostrarDialogSobre() {
+        AlertDialog.Builder(this)
+            .setTitle("Sobre l'aplicació")
+            .setMessage("Aplicació que gestiona les tasques")
+            .setPositiveButton("Tancar", null)
+            .show()
     }
     private fun initListeners() {
         btnTestNav.setOnClickListener {
--- app/src/main/java/com/example/act07_menu_llista_filtre/TasksTodayActivity.kt
+++ app/src/main/java/com/example/act07_menu_llista_filtre/TasksTodayActivity.kt
@@ -2,11 +2,14 @@ package com.example.act07_menu_llista_filtre
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,12 +20,56 @@ class TasksTodayActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MyAdapter
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks_today)
         setupRecyclerView()
+        setupToolbar()
         initComponents()
         initListeners()
+    }
+
+    private fun setupToolbar() {
+        toolbar = findViewById(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Les Meves Tasques"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_inici -> {
+                Toast.makeText(this, "Anant al Inici", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainUserActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_configuracio -> {
+                Toast.makeText(this, "Anant a configuració", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, ConfigurationActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.action_sobre -> {
+                mostrarDialogSobre()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun mostrarDialogSobre() {
+        AlertDialog.Builder(this)
+            .setTitle("Sobre l'aplicació")
+            .setMessage("Aplicació que gestiona les tasques")
+            .setPositiveButton("Tancar", null)
+            .show()
     }
 
     private fun setupRecyclerView() {
