22e00538220197c637e730fb41ee6a47848317d4
EstefaniaRubiano <rubianoestefania15@gmail.com>
2026-03-02 18:52:27 +0100

    actualizado el MyItem.kt para que coincida con la API y dependencias de Retrofit añadidas al gradle

--- app/src/main/java/com/example/projecte_android/MyItem.kt
+++ app/src/main/java/com/example/projecte_android/MyItem.kt
@@ -1,6 +1,11 @@
 package com.example.projecte_android
 
 data class MyItem(
+    val id: Int = 0,
     val title: String,
-    val category: String = "General"
+    val category: String = "General",
+    val imagePath: String? = null,
+    val completed: Boolean = false,
+    val dataCreated: String? = null,
+    val dataUpdated: String? = null
 )
